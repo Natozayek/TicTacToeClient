64b2a2820ee181f6989fd3aa80f1ad94c9c40d89
natozayek <MiguelAntonio.PenalverZayek@georgebrown.ca>
2022-11-09 14:32:43 -0500

    Loggin verification Reworked
    if player is connected and another person tries to logging with the same account will deny the access to server

--- Assets/Scripts/NetworkedClient.cs
+++ Assets/Scripts/NetworkedClient.cs
@@ -105,7 +105,7 @@ public class NetworkedClient : MonoBehaviour
             hostID = NetworkTransport.AddHost(topology, 0);
             Debug.Log("Socket open.  Host ID = " + hostID);
 
-            connectionID = NetworkTransport.Connect(hostID, "192.168.0.13", socketPort, 0, out error); // server is local on network
+            connectionID = NetworkTransport.Connect(hostID, "10.0.221.226", socketPort, 0, out error); // server is local on network
            
             Debug.Log(connectionID + "   -> cID.");
            
@@ -198,7 +198,7 @@ public class NetworkedClient : MonoBehaviour
 
 
             case 10:
-                message = 10;
+                message = 10;//Acces Denied
                 Debug.Log(stringMessage);
 
 
